94e8887457c9c2b220f6c44891072e67bd0bf51d
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-08-25 12:33:54 +0530

    create-next-app and app routing

---
+++ section1/app/about/layout.js
@@ -0,0 +1,13 @@
+export default function AboutLayout({ children }) {
+    return (
+       
+      <>
+        <h1>I am header for about</h1>
+        <div>
+            {children}
+        </div>
+        </>
+        
+    );
+  }
+  
---
+++ section1/app/login/hello/page.js
@@ -0,0 +1,7 @@
+export default function Hello() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between text-2xl font-bold  p-24">
+        Hello
+        </main>
+    );
+    }
